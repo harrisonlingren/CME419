5c85978c893d3ccefdc81bf90622e56d5a470a23
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-06 11:26:22 -0400

    fix bugs

--- class/test.php
+++ class/test.php
@@ -12,7 +12,9 @@
     $price = 9.98;
     $quantity = 2;
     $subtotal = $price * $quantity;
+    $SubtotalStr = number_format($subtotal, 2);
     $total = $subtotal * 1.07;
+    $totalStr = number_format($total, 2);
   ?>
 
   <hr />
@@ -20,22 +22,22 @@
 
   <ul>
     <li>
-      <?php echo 'Name: $firstName $lastname' ?>
+      <?php echo 'Name: ' + $firstName + ' ' + $lastname ?>
     </li>
     <li>
-      <?php echo 'Item: $color $item' ?>
+      <?php echo 'Item: ' + $color + ' ' + $item ?>
     </li>
     <li>
-      <?php echo 'Item Price: $price' ?>
+      <?php echo 'Item Price: ' + $price ?>
     </li>
     <li>
-      <?php echo 'Quantity: $quantity' ?>
+      <?php echo 'Quantity: ' + $quantity ?>
     </li>
     <li>
-      <?php echo 'Subtotal: $' + 'number_format($subtotal, 2)' ?>
+      <?php echo 'Subtotal: $' + $SubtotalStr ?>
     </li>
     <li>
-      <?php echo 'Total: $' + 'number_format($total, 2)' ?>
+      <?php echo 'Total: $' + $totalStr ?>
     </li>
   </ul>
 
