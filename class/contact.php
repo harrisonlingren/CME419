3749a24897b1d6e21a1200750ff830c9b30fcfbb
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-19 13:25:44 -0400

    form homework

--- class/contact.php
+++ class/contact.php
@@ -9,10 +9,47 @@
     <input type="text" placeholder="First Name" name="fName" id="fName" /><br />
     <input type="text" placeholder="Last Name" name="lName" id="lName" /><br />
     <input type="text" placeholder="Email" name="email" id="email" /><br />
+
+    <select name="birthMonth" placeholder="Month" id="birthMonth" />
+      <?php
+        $months = array('January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December');
+        for ($x=0;$x<11;$x++) {
+          echo '<option value="$x">' . $months[$x] . '</option>';
+        }
+      ?>
+    </select>
+
+    <select name="birthDay" placeholder="Day" id="birthDay" />
+      <?php
+        for ($x=1;$x=31;$x++) {
+          echo '<option value="$x">$x</option>';
+        }
+      ?>
+    </select>
+
+    <select name="birthYear" placeholder="Year" id="birthYear" />
+      <?php
+        for ($x=1980;$x<2010;$x++) {
+          echo '<option value="$x">$x</option>';
+        }
+      ?>
+    </select>
+
     <select name="gender" id="gender">
       <option value="Male">Male</option>
       <option value="Female">Female</option>
     </select><br />
+
+    <select name="major" placeholder="Major..." id="major">
+      <<?php
+        $majors = array('CSD', 'CME', 'CCM', 'HCOL', 'Journalism', 'StratCom');
+        foreach ($majors as $major) {
+          echo '<option value="$major">$major</option>';
+        }
+      ?>
+    </select>
+
     <input type="text" placeholder="Majors" name="majors" id="majors" /><br />
     <input type="text" placeholder="Comments" name="comments" id="comments" /><br /><br />
     <button type="submit">Submit</button>
