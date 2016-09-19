fea09836213307a8a8b12aa036572c0e9bf456b8
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-19 13:46:11 -0400

    form homework

--- class/procForm.php
+++ class/procForm.php
@@ -6,21 +6,24 @@
 
 <body>
   <?php
-  $firstName = $_POST['fName'];
-  $lastName = $_POST['lName'];
-  $email = $_POST['email'];
-  $birthMonth = $_POST['birthMonth'];
-  $birthDay = $_POST['birthDay'];
-  $birthYear = $_POST['birthYear'];
-  $gender = $_POST['gender'];
-  $major = $_POST['major'];
-  $comments = $_POST['comments'];
-  $fullName = $firstName . " " . $lastName;
+    $firstName = $_POST['fName'];
+    $lastName = $_POST['lName'];
+    $email = $_POST['email'];
+    $birthMonth = $_POST['birthMonth'];
+    $birthDay = $_POST['birthDay'];
+    $birthYear = $_POST['birthYear'];
+    $gender = $_POST['gender'];
+    $major = $_POST['major'];
+    $comments = $_POST['comments'];
+    $fullName = $firstName . " " . $lastName;
 
-  echo "<p>Thank you, $fullName for submitting your information. The following
-  information was obtained from your form:</p>";
+    $msg = "<p>Thank you, $fullName for submitting your information. The following
+    information was obtained from your form:</p>
+    <br /><br />
+    Email: $email <br />Birth date: $birthMonth $birthDay, $birthYear <br />Gender: $gender <br />Major(s): $major <br />Comments: $comments";
 
-  echo "Email: $email <br />Birth date: $birthMonth $birthDay, $birthYear <br />Gender: $gender <br />Major(s): $major <br />Comments: $comments";
+    mail($email, "Your submission", $msg);
+    echo $msg;
   ?>
 </body>
 
