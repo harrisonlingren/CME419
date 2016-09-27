d60ce94a90c688ecc36f3a5b42eb184fe9169211
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-27 11:51:56 -0400

    add missing semicolon and add lines between forms

--- assignments/functions/index.php
+++ assignments/functions/index.php
@@ -4,19 +4,9 @@
 </head>
 <body>
   <?php
-    require('area.php'); echo "\n";
-    require('mpg.php'); echo "\n";
-    require('bmi.php'); echo "\n";
+    require('area.php'); echo "<br />";
+    require('mpg.php'); echo "<br />";
+    require('bmi.php'); echo "<br />";
   ?>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
- ?>
