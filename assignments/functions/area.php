773908961a2e54e782dac48d749eb95e526d9db1
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-27 13:14:01 -0400

    finish area and mpg forms

--- assignments/functions/area.php
+++ assignments/functions/area.php
@@ -1,5 +1,35 @@
+<?php
+  function getArea($h, $w) {
+    $area = $w * $h;
+    return number_format($area, 2, '.', '');
+  }
+
+  $htErr = "";
+  $wtErr = "";
+?>
+
 <form action="" method="post">
-  <label for="aWidth">Width</label><input type="text" id="aWidth" /><br />
-  <label for="aHeight">Height</label><input type="text" id="aHeight" /><br /><br />
+  <label for="aWidth">Width</label><input type="text" name="aWidth" id="aWidth" /><br />
+  <label for="aHeight">Height</label><input type="text" name="aHeight" id="aHeight" /><br /><br />
   <button type="submit">Find Area</button>
 </form>
+
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["aWidth"])) {
+      $wtErr = "Width value is required";
+    } else {
+      $width = $_POST['aWidth'];
+    }
+
+    if (empty($_POST["aHeight"])) {
+      $htErr = "Weight value is required";
+    } else {
+      $height = $_POST['aHeight'];
+    }
+
+    $height = ($feet * 12) + $inches;
+    echo "<h3>BMI is: " . getArea($height, $width) . "</h3>";
+    echo "<br />$htErr<br />$wtErr<br />";
+  }
+?>
--- assignments/functions/mpg.php
+++ assignments/functions/mpg.php
@@ -1,9 +1,54 @@
+<?php
+  function getMpg($m, $g) {
+    $mpg = $m / $g;
+    return number_format($mpg, 2, '.', '');
+  }
+
+  function getBool($m, $r) {
+    switch ($r) {
+      case 'bad':
+        if ($m < 18) {return true;}
+      case 'avg':
+        if ($m > 17 && $m < 27) {return true;}
+      case 'good':
+        if ($m > 26) {return true;}
+      default:
+        return false;
+        break;
+    }
+  }
+
+  $miErr = "";
+  $galErr = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["miles"])) {
+      $miErr = "Miles value is required";
+    } else {
+      $miles = $_POST['miles'];
+    }
+
+    if (empty($_POST["gallons"])) {
+      $galErr = "Gallons value is required";
+    } else {
+      $inches = $_POST['gallons'];
+    }
+
+  $mpg = getMpg($miles, $gallons);
+?>
+
 <form action="" method="post">
-  <label for="miles">Miles Driven</label><input type="text" id="miles" /><br />
-  <label for="gallons">Gallons of Gas Used</label><input type="text" id="gallons" /><br /><br />
+  <label for="miles">Miles Driven</label><input type="text" id="miles" name="miles" /><br />
+  <label for="gallons">Gallons of Gas Used</label><input type="text" id="gallons" name="gallons" /><br /><br />
   <label>Car Efficiency</label>
-  <input type="radio" id="bad" /><label for="bad">Bad Efficiency</label><br />
-  <input type="radio" id="avg" /><label for="avg">Average Efficiency</label><br />
-  <input type="radio" id="good" /><label for="good">Good Efficiency</label><br /><br />
+  <input type="radio" id="bad" <?php echo "selected=" . getBool($mpg, "bad"); ?> /><label for="bad">Bad Efficiency</label><br />
+  <input type="radio" id="avg" <?php echo "selected=" . getBool($mpg, "avg"); ?> /><label for="avg">Average Efficiency</label><br />
+  <input type="radio" id="good" <?php echo "selected=" . getBool($mpg, "good"); ?> /><label for="good">Good Efficiency</label><br /><br />
   <button type="submit">Check Efficiency</button>
 </form>
+
+<?php
+    echo "<h3>MPG is: $mpg</h3>";
+    echo "<br />$miErr<br />$galErr";
+  }
+?>
