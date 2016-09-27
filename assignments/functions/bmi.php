18b94ef14fa72685d709ac4b01db9bc64f4d2753
Harrison Lingren <harrisonlingren@gmail.com>
2016-09-27 14:06:28 -0400

    finishing touches

--- assignments/functions/bmi.php
+++ assignments/functions/bmi.php
@@ -23,7 +23,7 @@
 
 <form action="" method="post">
   <label>Width</label><input type="text" name="feet" placeholder="feet" value="<?php echo $feet;?>"/><input type="text" name="inches" value="<?php echo $inches;?>"placeholder="inches" /><br />
-  <label>Height</label><input type="text" name="weight" value="<?php echo $weight;?>"/><br /><br />
+  <label>Weight</label><input type="text" name="weight" value="<?php echo $weight;?>"/><br /><br />
   <button type="submit">Calculate BMI</button>
 </form>
 
