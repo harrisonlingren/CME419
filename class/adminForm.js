ca37c9e50c7dc64091d3080ad500b913d0263aa6
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-03 13:31:33 -0400

    db connect stuff

---
+++ class/adminForm.js
@@ -0,0 +1,24 @@
+document.getElementById('addDiv').style.display="block";
+document.getElementById('updateDiv').style.display="none";
+document.getElementById('deleteDiv').style.display="none";
+				
+var ddValue = document.getElementById('adminChoice');
+ddValue.onchange = newChoice;
+			
+function newChoice() {
+	var adminChoice = document.getElementById('adminChoice').value;
+			
+	if(adminChoice == "add") {
+		document.getElementById('addDiv').style.display="block";
+		document.getElementById('updateDiv').style.display="none";
+		document.getElementById('deleteDiv').style.display="none";
+	} else if(adminChoice=="update") {
+		document.getElementById('updateDiv').style.display="block";
+		document.getElementById('addDiv').style.display="none";
+		document.getElementById('deleteDiv').style.display="none";
+	} else {
+		document.getElementById('deleteDiv').style.display="block";
+		document.getElementById('addDiv').style.display="none";
+		document.getElementById('updateDiv').style.display="none";
+	}
+	};
---
+++ class/footer.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title><?php echo 'title' ?></title>
+  <script src="adminForm.js"></script>
+</head>
+<body>
+  <header>
+    <ul>
+      <li><a href="list.php">List</a></li>
+      <li><a href="admin.php">Administration</a></li>
+    </ul>
+  </header>
+</body>
+</html>
