ea1470e3e6e9888ef7936d3c3caed17dfde667e3
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-10 19:49:03 -0500

    fix another goddamn typo

--- class/admin.php
+++ class/admin.php
@@ -40,7 +40,7 @@ require('db_connect.php');
     $run_query = mysqli_query($dbc, $populate_query);
 
     if($run_query) {
-      echo "<table><tr><th>Street Address</th><th>City</th><th>State</th></tr></table>";
+      echo "<table><tr><th>Street Address</th><th>City</th><th>State</th></tr>";
       while($row=mysqli_fetch_array($run_query, MYSQLI_ASSOC)) {
         echo '<tr><td>' . $row["street"] . '</td><td>' . $row["city"] . '</td><td>' . $row["state"] . '</td>';
         echo '<td><input type="button" id="' . $row["property_id"] . '" name="property" value=""/></td></tr>';
