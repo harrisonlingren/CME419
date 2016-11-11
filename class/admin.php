f5064b04c645466c4fd1d3aad58f34a38722b11d
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-10 21:06:42 -0500

    change edit/delete buttons to use class

--- class/admin.php
+++ class/admin.php
@@ -100,7 +100,8 @@ require('states.php');
       echo "<table><tr><th>Street Address</th><th>City</th><th>State</th><th></th></tr>";
       while($row=mysqli_fetch_array($run_query, MYSQLI_ASSOC)) {
         echo '<tr><td>' . $row["street"] . '</td><td>' . $row["city"] . '</td><td>' . $row["state"] . '</td>';
-        echo '<td><input type="button" id="edit-' . $row["property_id"] . '" name="property" value="Edit"/><input type="button" id="delete-' . $row["property_id"] . '" name="property" value="Delete"/></td></tr>';
+        echo '<td><input type="button" id="' . $row["property_id"] . '" class="editBtn" name="property" value="Edit"/>';
+        echo '<input type="button" id="' . $row["property_id"] . '" class="deleteBtn" name="property" value="Delete"/></td></tr>';
       }
       echo '</table>';
     } else {
