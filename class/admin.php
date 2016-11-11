2c7440fbfaa44c91cb677c408a7ccffc1c38a800
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-10 19:28:01 -0500

    admin page

--- class/admin.php
+++ class/admin.php
@@ -21,6 +21,31 @@ require('db_connect.php');
   </div>
   <div id="updateDiv">
     This is the Update Property
+
+    <?php
+    if ($dbc) {
+      echo "connected!<br />";
+    } else {
+      echo "not connected!<br />";
+    }
+
+    $populate_table = "select street, city, state from location where type_id=1";
+    $check_query = mysqli_query($dbc, $populate_table);
+
+    if($check_query) {
+      echo "<table><tr><th>Street Address</th><th>City</th><th>State</th></tr></table>";
+      while($row=mysqli_fetch_array($r1, MYSQLI_ASSOC)) {
+        echo '<tr><td>' . $row["street"] . '</td><td>' . $row["city"] . '</td><td>' . $row["state"] . '</td></tr>';
+        echo '<td><input type="button" id="' . $row["property_id"] . '" name="property" value=""/></td>';
+      }
+      echo '</table>';
+    } else {
+      echo $result;
+      echo 'Could not load query: ' . $populate_table;
+    }
+    ?>
+
+
   </div>
   <div id="deleteDiv">
     This is the Delete Property
