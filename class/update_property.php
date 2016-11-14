5ecb928f8d8dbfeda15a7c791453702a356437b1
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-14 15:05:05 -0500

    finishing adding echos

--- class/update_property.php
+++ class/update_property.php
@@ -11,6 +11,8 @@ echo "<h3>ID: $prop</h3>";
 
 <h2>Admin Portal</h2>
 <form method="POST" action="update_submit.php">
+
+  <!-- Location fields -->
   <?php
     $get_loc_data = "select distinct location.street, location.city, school.zip, school.county from location left join school on (location.zip = school.zip) where type_id = 0 and location.property_id = $prop";
     $loc_data_query = mysqli_query($dbc, $get_loc_data);
@@ -45,6 +47,7 @@ echo "<h3>ID: $prop</h3>";
     <input type="text" id="addCounty" name="addCounty" required value="<?php echo $loc_data['county']; ?>"/><br/>
   </fieldset>
 
+  <!-- Property fields -->
   <?php
     $get_prop_data = "select bed, bath, garage, pets, amenities, details.desc from res_info inner join details using (property_id) where res_info.property_id = $prop";
     $prop_data_query = mysqli_query($dbc, $get_prop_data);
@@ -66,7 +69,7 @@ echo "<h3>ID: $prop</h3>";
     <input type="text" id="addGarage" name="addGarage" required value="<?php echo $prop_data['garage']; ?>"/><br/>
     <label>Are pets allowed?</label>
     <?php
-      if ($prop_data['pets']) {
+      if ($prop_data['pets'] == 1) {
         echo '<input type="radio" id="noPets" name="addPets" value="0" required />
         <label for="noPets">No</label>
         <input type="radio" id="yesPets" name="addPets" value="1" required checked/>
@@ -84,38 +87,65 @@ echo "<h3>ID: $prop</h3>";
     <textarea id="addDesc" name="addDesc"><?php echo $prop_data['garage']; ?></textarea><br/>
   </fieldset>
 
+  <!-- School fields -->
   <?php
     $get_school_data = "select elementary, middle, high from school where location.property_id = $prop";
     $school_data_query = mysqli_query($dbc, $get_school_data);
-    $school_data = mysqli_fetch_array($school_data_query, MYSQLI_ASSOC);
+    if ($school_data_query) {
+      $school_data = mysqli_fetch_array($school_data_query, MYSQLI_ASSOC);
+    } else {
+      echo "Could not fetch data for query: $school_data_query";
+    }
   ?>
   <fieldset>
     <legend>School Information</legend>
     <label for="elem">Elementary School:</label>
-    <input type="text" for="elem" name="elem" required/><br/>
+    <input type="text" for="elem" name="elem" required value="<?php echo $school_data['elementary']; ?>"/><br/>
     <label for="middle">Middle School:</label>
-    <input type="text" for="middle" name="middle" required/><br/>
+    <input type="text" for="middle" name="middle" required value="<?php echo $school_data['middle']; ?>"/><br/>
     <label for ="high">High School:</label>
-    <input type="text" for "high" name="high" required/><br/>
+    <input type="text" for "high" name="high" required value="<?php echo $school_data['high']; ?>"/><br/>
   </fieldset>
 
+  <!-- Rent fields -->
   <?php
     $get_rent_data = "select rent, availability, status from rent where property_id = $prop";
     $rent_data_query = mysqli_query($dbc, $get_rent_data);
-    $rent_data = mysqli_fetch_array($rent_data_query, MYSQLI_ASSOC);
+    if ($rent_data_query) {
+      $rent_data = mysqli_fetch_array($rent_data_query, MYSQLI_ASSOC);
+    } else {
+      echo "Could not fetch data for query: $rent_data_query";
+    }
   ?>
   <fieldset>
     <legend>Property Rent</legend>
     <label for="rent">Rent:</label>
-    <input type="number" for="rent" name="rent" required/><br/>
+    <input type="number" for="rent" name="rent" required value="<?php echo $rent_data['rent']; ?>"/><br/>
     <label>Rental Status:</label>
+
+    <?php
+      if ($rent_data['status'] == "0") {
+        echo '<input type="radio" id="available" name="rentStatus" value="0" required checked/>
+        <label for="available">Available</label>
+        <input type="radio" id="leased" name="rentStatus" required value="1"/>
+        <label for="leased">Leased</label><br/>';
+      } else {
+        echo '<input type="radio" id="available" name="rentStatus" value="0" required/>
+        <label for="available">Available</label>
+        <input type="radio" id="leased" name="rentStatus" value="1" required checked/>
+        <label for="leased">Leased</label><br/>';
+      }
+    ?>
+
     <input type="radio" id="available" name="rentStatus" value="0" required/>
     <label for="available">Available</label>
     <input type="radio" id="leased" name="rentStatus" value="1"/>
     <label for="leased">Leased</label><br/>
+
     <label for="availDate">Available Date:</label>
-    <input type="date" id="availDate" name="availDate" required/><br/>
+    <input type="date" id="availDate" name="availDate" required value="<?php echo $rent_data['availability']; ?>"/><br/>
   </fieldset>
+
   <input type="submit" value="Update Property" name="updateSubmit" id="updateSubmit"/>
   <input type="button" value="Cancel" id="cancelButton" name="cancelButton" onclick="leavePage()" />
 </form>
