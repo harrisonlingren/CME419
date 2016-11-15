310479630dc17ae4bafd35893d649b9e71e15591
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-15 13:52:02 -0500

    fix state dropdown logic

--- class/update_property.php
+++ class/update_property.php
@@ -33,7 +33,7 @@ echo "<h3>ID: $prop</h3>";
     <select name="addState" id="addState" required>
       <?php
         foreach($states as $key => $value) {
-          echo $key . ": " . $loc_data;
+          echo $key . ": " . $loc_data['state'];
           if ($key == $loc_data['state']) {
             echo '<option value="' . $key . '" selected="selected">' . $value . '</option>';
           } else {
