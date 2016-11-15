b6acc491d9f205a52fdc7eb814141f4a191aeca6
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-15 13:30:06 -0500

    fix errors in update handler

--- class/update_submit.php
+++ class/update_submit.php
@@ -18,11 +18,11 @@ $desc = $_POST['addDesc'];
 $update_location = "UPDATE location SET street='$street', city='$city', state='$state', zip='$zip' WHERE property_id = $prop\n";
 $update_property = "UPDATE res_info SET bed='$bed', bath='$bath', garage='$garage', pets='$pets' WHERE property_id = $prop\n";
 $update_rental = "UPDATE rent SET rent='$rent', availability='$availability', status='$status' WHERE property_id = $prop\n";
-$update_details = "UPDATE details SET amenities='$amenities', details.desc='$desc' WHERE property_id = $prop\n";
+$update_details = "UPDATE details SET amenities='$amenities', details.desc='$desc' WHERE property_id = $prop";
 
-echo $update_location;
-echo $update_property;
-echo $update_rental;
+echo $update_location . '<br />';
+echo $update_property . '<br />';
+echo $update_rental . '<br />';
 echo $update_details;
 
 ?>
