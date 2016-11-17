38fdc88faa7c25f4c486de5014e796fa2a41215a
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-17 13:18:57 -0500

    delete confirmation

--- class/admin.php
+++ class/admin.php
@@ -79,7 +79,7 @@ require('states.php');
 </div>
 
 <div id="updateDiv">
-  <div id="tableDiv">
+  <div id="tableDivUpdate">
     <?php
       if ($dbc) {
         echo "<br />connected!<br />";
@@ -108,7 +108,32 @@ require('states.php');
 </div>
 
 <div id="deleteDiv">
+  <div id="tableDivDelete">
+    <?php
+      if ($dbc) {
+        echo "<br />connected!<br />";
+      } else {
+        echo "<br />not connected!<br />";
+      }
 
+      $populate_query = "select * from location where type_id = 0";
+      $run_query = mysqli_query($dbc, $populate_query);
+
+      if($run_query) {
+        echo '<form action="delete_property.php" method="post">
+        <input type="submit" value="Delete selected property" id="deleteSubmit"/>
+        <table><tr><th>Delete?</th><th>Street Address</th><th>City</th><th>State</th></tr>';
+        while($row=mysqli_fetch_array($run_query, MYSQLI_ASSOC)) {
+          echo '<tr><td><input type="radio" name="updateID" id="' . $row['property_id'] . '" value="' . $row['property_id'] . '" /></td>';
+          echo '<td>' . $row["street"] . '</td><td>' . $row["city"] . '</td><td>' . $row["state"] . '</td></tr>';
+        }
+        echo '</table></form>';
+      } else {
+        echo $result;
+        echo 'Could not load query: ' . $populate_query;
+      }
+    ?>
+  </div>
 </div>
 
 <?php include('footer.php'); ?>
