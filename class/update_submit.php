f3505c37a3a7c838fc1a536da1bcaaa198248b01
Harrison Lingren <harrisonlingren@gmail.com>
2016-11-17 13:52:37 -0500

    rewrite update submit

--- class/update_submit.php
+++ class/update_submit.php
@@ -17,28 +17,36 @@ $availability = $_POST['availDate'];
 $status = $_POST['rentStatus'];
 $amenities = $_POST['addAmenities'];
 $desc = $_POST['addDesc'];
+$elementary = $_POST['elem'];
+$middle = $_POST['middle'];
+$high = $_POST['high'];
+$county = $_POST['addCounty'];
 
-$update_location = "UPDATE location SET street='$street', city='$city', state='$state', zip='$zip' WHERE property_id = $prop\n";
-$update_property = "UPDATE res_info SET bed='$beds', bath='$bath', garage='$garage', pets='$pets' WHERE property_id = $prop\n";
-$update_rental = "UPDATE rent SET rent='$rent', availability='$availability', status='$status' WHERE property_id = $prop\n";
-$update_details = "UPDATE details SET amenities='$amenities', details.desc='$desc' WHERE property_id = $prop";
-
-echo $update_location . '<br />';
-echo $update_property . '<br />';
-echo $update_rental . '<br />';
-echo $update_details . '<br />';
-
+$prop = $_POST['updateID'];
+$flag = TRUE;
 mysqli_autocommit($dbc, false);
-mysqli_query($dbc, $update_location);
-mysqli_query($dbc, $update_property);
-mysqli_query($dbc, $update_rental);
-mysqli_query($dbc, $update_details);
 
-$result = $mysqli_commit($dbc);
-if ($result) {
-  echo $result . ": transaction succeeded!";
+$update_queries = array();
+$update_queries['location'] = "UPDATE location SET street='$street', city='$city', state='$state', zip='$zip' WHERE property_id = $prop";
+$update_queries['res_info'] = "UPDATE res_info SET bed='$beds', bath='$bath', garage='$garage', pets='$pets' WHERE property_id = $prop";
+$update_queries['rent'] = "UPDATE rent SET rent='$rent', availability='$availability', status='$status' WHERE property_id = $prop";
+$update_queries['school'] = "UPDATE school SET high='$high', middle='$middle', elementary='$elementary', county='$county' WHERE property_id = $prop";
+$update_queries['details'] = "UPDATE details SET amenities='$amenities', details.desc='$desc' WHERE property_id = $prop";
+
+foreach($update_queries as $table=>$query) {
+  $run_update = mysqli_query($dbc, $query);
+  if (!$run_update) {
+    $flag = false;
+    echo "<br /><br /><b><i>Error on table:</i> $table</b> Details below:<br />" . mysqli_error($dbc);
+  }
+}
+
+if ($flag) {
+  if (mysqli_commit($dbc)) {
+    echo "<br /><hr /><br /><h3>Success!</h3>";
+  }
 } else {
-  echo $result . ": transaction failed.";
+  echo "<br /><hr /><br /><h3>Property #$prop could not be updated. Please see your administrator for assistance.</h3>";
 }
 
 ?>
