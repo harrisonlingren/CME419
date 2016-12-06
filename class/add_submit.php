027478cee385b1428da0a9844e7704afb4354534
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-06 13:26:39 -0500

    refactor add form handling

--- class/add_submit.php
+++ class/add_submit.php
@@ -34,76 +34,48 @@ $testDetails="INSERT INTO details (amenities, details.desc) VALUES ('$amenity', 
 $testSchool="INSERT INTO school (county, elementary, middle, high) VALUES ('$county', '$elem', '$middle', '$high')";
 $testRent="INSERT INTO rent (rent, status, availability) VALUES ('$rent', '$status', '$availDate')";
 
-$resultRun = mysqli_query($dbc, $testLocation);
+$query_array = array($testLocation, $testResidential, $testDetails, $testSchool, $testRent);
 
-if (!$resultRun) {
-   $flag = false;
+foreach ($query_array as $q) {
+  $resultRun = mysqli_query($dbc, $q);
+
+  if (!$resultRun) {
+    $flag = false;
     echo "Error details: " . mysqli_error($dbc);
-};
-
-$resultRun = mysqli_query($dbc, $testResidential);
-if (!$resultRun) {
-   $flag = false;
-    echo "Error details: " . mysqli_error($dbc);
-};
-
-$resultRun = mysqli_query($dbc, $testDetails);
-if (!$resultRun) {
-   $flag = false;
-    echo "Error details: " . mysqli_error($dbc);
-};
-
-$resultRun = mysqli_query($dbc, $testSchool);
-if (!$resultRun) {
-   $flag = false;
-    echo "Error details: " . mysqli_error($dbc);
-};
-
-$resultRun = mysqli_query($dbc, $testRent);
-if (!$resultRun) {
-   $flag = false;
-    echo "Error details: " . mysqli_error($dbc);
-};
-
-mysqli_rollback($dbc);
-
+  }
+}; mysqli_rollback($dbc);
 
 if($flag) {
 	mysqli_autocommit($dbc, true);
-	$result = mysqli_query($dbc, $testLocation);
-	if($result) {
-	$getID="SELECT property_id FROM rental_location WHERE street='$street'";
-	$resultID = mysqli_query($dbc, $getID);
-	$row=mysqli_fetch_array($resultID, MYSQLI_ASSOC);
-	$newPropertyID=$row['property_id'];
 
+  $result = mysqli_query($dbc, $testLocation);
+	if($result) {
+
+  	$getID="SELECT property_id FROM rental_location WHERE street='$street'";
+  	$resultID = mysqli_query($dbc, $getID);
+  	$row=mysqli_fetch_array($resultID, MYSQLI_ASSOC);
+  	$newPropertyID=$row['property_id'];
+
+    if(!empty($newPropertyID)) {
+      $addResidential="INSERT INTO res_info (property_id, bed, bath, garage, pets) VALUES ('$newPropertyID', '$bed', '$bath', '$garage', '$pets')";
+  		$addDetails="INSERT INTO details (property_id, amenities, details.desc) VALUES ('$newPropertyID','$amenity', '$desc')";
+  		$addSchool="INSERT INTO school (property_id, county, elementary, middle, high) VALUES ('$newPropertyID','$county', '$elem', '$middle', '$high')";
+  		$addRent="INSERT INTO rent (property_id, rent, status, availability) VALUES ('$newPropertyID','$rent', '$status', '$availDate')";
+
+      $add_array = array($addResidential, $addDetails, $addSchool, $addRent);
+      foreach ($add_array as $q) {
+        $exec_q = mysqli_query($dbc, $q);
+      }
+    }
 	} else {
 		echo "This did not work." . mysqli_error($dbc);
-	};
-	echo $newPropertyID;
-	//run all the queries
-	if(!empty($newPropertyID)) {
-		$addResidential="INSERT INTO res_info (property_id, bed, bath, garage, pets) VALUES ('$newPropertyID', '$bed', '$bath', '$garage', '$pets')";
-
-		$addDetails="INSERT INTO details (property_id, amenities, details.desc) VALUES ('$newPropertyID','$amenity', '$desc')";
-
-		$addSchool="INSERT INTO school (property_id, county, elementary, middle, high) VALUES ('$newPropertyID','$county', '$elem', '$middle', '$high')";
-
-		$addRent="INSERT INTO rent (property_id, rent, status, availability) VALUES ('$newPropertyID','$rent', '$status', '$availDate')";
-
-
-		$result2=mysqli_query($dbc, $addDetails);
-		$result3=mysqli_query($dbc, $addResidential);
-		$result4=mysqli_query($dbc, $addRent);
-		$result5=mysqli_query($dbc, $addSchool);
 	}
-
 
 
 
 	//mysqli_commit($dbc);
 	echo "<p>The following property and property details have been added to the database:</p>";
-	echo "<h3>$street, $city, $state  $zip</h3>";
+	echo "<h3>Property #$newPropertyID: $street, $city, $state  $zip</h3>";
 	echo "<p>Bed(s): $bed<br/>Bathroom(s): $bath</br>Garage: $garage<br/>Pets Allowed: $pets<br/>";
 	echo "Amenities: $amenity<br/>Description: $desc<br>Elementary School: $elem<br/>Middle School: $middle<br/>";
 	echo "High School: $high<br/>County: $county</p>";
