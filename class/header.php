2ac1c0cd14112fec0901c6de348b48c484d501a4
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 12:22:19 -0500

    session work

--- class/header.php
+++ class/header.php
@@ -16,5 +16,14 @@
       <li><a href="list.php">List</a></li>
       <li><a href="search.php">Search</a></li>
       <li><a href="admin.php">Administration</a></li>
+
+      <?php
+        if (!isset($_SESSION['firstname'])) {
+          echo '<li><a href="login.php">Log in</a></li>';
+        } else {
+          echo '<li><a href="loggedout.php">Log out</a></li>';
+        }
+      ?>
+      
     </ul>
   </header>
