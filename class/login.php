d4ec172ad123b4f3e3e30672d5389c3cc179a474
Harrison Lingren <harrisonlingren@gmail.com>
2016-12-07 12:19:30 -0500

    session work

--- class/login.php
+++ class/login.php
@@ -2,10 +2,10 @@
 
 <form action="loggedin.php" method="POST">
 
-  <label for="user">Username</label><br />
+  <label for="user">Username</label>
   <input type="text" name="user" id="user" /> <br />
 
-  <label for="pass">Password</label><br />
+  <label for="pass">Password</label>
   <input type="password" name="pass" id="pass" /><br /><br />
 
   <button type="submit">Submit</button>
